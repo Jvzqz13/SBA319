ceff261ffcb71115d788ff2c4e2ab6cfc07e6f6e
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2024-01-20 20:49:24 -0500

    comedian schema

---
+++ backend/models/comedian.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const comedianSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    pic:{
+        type: String
+    },
+    bio:{
+        type: String
+    },
+    specials:{
+        type: String
+    },
+    podcasts:{
+        type: String
+    }, 
+    showdates: {
+        type: Date
+    }
+})
+
+export default mongoose.model('Comedian', comedianSchema)
