8700189a260f4374c8a19f9e7abda715e961e35e
Jvzqz13 <142378858+Jvzqz13@users.noreply.github.com>
2024-01-22 20:05:21 -0500

    added put and deleted routes

--- backend/routes/comedians.js
+++ backend/routes/comedians.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-//GET - GET USER BY ID
+//GET - GET COMEDIAN BY ID
 router.get('/:id', async (req, res) => {
     try {
        const comedian_id = await Comedians.find(req.params.id);
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-//GET - GET USER BY NAME
+//GET - GET COMEDIAN BY NAME
 router.get('/name/:id', async (req, res) => {
     try {
         const name = await Comedians.findOne({"name": req.params.id});
@@ -42,6 +42,30 @@ router.get('/name/:id', async (req, res) => {
         else res.status(200).json(name);
     } catch (e){
         console.log(e);
+    }
+})
+
+//PUT - UPDATE A COMEDIAN BY ID
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { body } = req.params;
+        const updateComedian = await Comedians.findByIdAndUpdate(id, body, {new: true});
+        res.status(201).json({updateComedian})
+
+    } catch (e) {
+        console.log(e.message);
+    }
+})
+
+//DELETE - DELETE COMEDIAN BY ID
+router.delete('/:id', async (req, res) => {
+    try{ 
+        const { id } = req.params;
+        const deletedComedian = await Comedians.findByIdAndDelete(id);
+        res.json({msg: `Comedian ${deletedComedian.id} was Deleted`})
+    } catch (e){
+        console.log(e.message);
     }
 })
 
